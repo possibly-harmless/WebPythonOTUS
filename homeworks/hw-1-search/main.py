a5f0aa950663fab7d37972fa6b503bb30ec374bb
Leonid Shifrin <shifrinl@yandex.ru>
2020-11-29 22:41:27 +0300

    Start using click

--- homeworks/hw-1-search/main.py
+++ homeworks/hw-1-search/main.py
@@ -1,29 +1,97 @@
 import search.drivers  # Need this to load / register drivers
+import click
 from search.linkextractor import SEDriverRegistry
 from search.results import ResultsHandler
-from search.logger import SearchLogger
+from search.logger import SearchLogger, DEFAULT_LOG_PATH
 
+
+DEFAULT_MAX_RESULTS = 30
+DEFAULT_MAX_RECURSION_DEPTH = 5
+DEFAULT_CONSOLE_FLAG = True
+DEFAULT_SEARCH_ENGINE = "google"
+DEFAULT_SEARCH_MODE = "all"
+DEFAULT_VERBOSE_FLAG = False
+DEFAULT_LOG_LEVEL = 'info'
+
+REGISTERED_ENGINES = SEDriverRegistry.registered_drivers_names()
+SUPPORTED_SEARCH_MODES = ('any', 'all')
+SUPPORTED_LOG_LEVELS = SearchLogger.log_level_mappings().keys()
+
+@click.command()
+@click.argument("query")
+@click.option(
+    "--engine",
+    default=DEFAULT_SEARCH_ENGINE,
+    type=click.Choice(REGISTERED_ENGINES),
+    help=f"Search engine type"
+)
+@click.option(
+    "--limit",
+    default=DEFAULT_MAX_RESULTS,
+    help="Max number of results to return"
+)
+@click.option(
+    "--console/--no-console",
+    default=DEFAULT_CONSOLE_FLAG,
+    # is_flag=True,
+    help="Whether or not to print results to console. Defaults to --console"
+)
+@click.option(
+    "--mode",
+    default=DEFAULT_SEARCH_MODE,
+    type=click.Choice(SUPPORTED_SEARCH_MODES),
+    help="""Keep only links with all or any of the words in the query. Defaults to 'all' """
+)
+@click.option(
+    "--depth_limit",
+    default=DEFAULT_MAX_RECURSION_DEPTH,
+    help="Recursion depth limit. Defaults to 5"
+)
+@click.option(
+    "--resultpath",
+    default=None,
+    help="A path to .csv or .json file to save the results to. Defaults to None"
+)
+@click.option(
+    "--verbose/--brief",
+    default=DEFAULT_VERBOSE_FLAG,
+    is_flag=True,
+    help="""Whether or not to keep extended search information in results."""
+)
+@click.option(
+    "--logpath",
+    default=DEFAULT_LOG_PATH,
+    help=f"Path to log file. Defaults to {DEFAULT_LOG_PATH}"
+)
+@click.option(
+    "--loglevel",
+    default=DEFAULT_LOG_LEVEL,
+    type=click.Choice(SUPPORTED_LOG_LEVELS),
+    help="Sets the log level. Defaults to 'info'"
+)
+def search(query, engine, limit, console, mode, depth_limit, resultpath, verbose, logpath, loglevel):
+
+    SearchLogger.init_logger(path=logpath, log_to_console=True, level=loglevel)
+    logger = SearchLogger.get_logger()
+
+    extractor = SEDriverRegistry.get_driver(engine)
+
+    results = list(extractor.recursive_link_generator(
+        query,
+        limit=limit,
+        search_mode=mode,
+        depth_limit=depth_limit
+    ))
+
+    logger.info("Finished search...")
+
+    if console:
+        ResultsHandler.console_print(results, verbose=verbose)
+
+    if resultpath:
+        ResultsHandler.save_results(results, resultpath, verbose=verbose)
 
 
 if __name__ == "__main__":
-    extractor = SEDriverRegistry.get_driver("google")
-    logger = SearchLogger.get_logger()
-    glgen = extractor.recursive_link_generator(
-        "python generator",
-        limit=30,
-        search_mode="all",
-        depth_limit=5
-    )
-    results = list(glgen)
-    ResultsHandler.console_print(results, verbose=False)
-    ResultsHandler.save_to_csv(
-        results,
-        path="/Users/archie/Temp/search_results.csv",
-        verbose=False
-    )
-    ResultsHandler.save_to_json(
-        results,
-        path="/Users/archie/Temp/search_results.json",
-        verbose=False
-    )
+    search()
 
