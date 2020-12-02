1665d58851117febbbc8354e4e3d3d3271bb0094
Leonid Shifrin <shifrinl@yandex.ru>
2020-12-02 17:15:10 +0300

    Added setup.py (untested so far)

---
+++ homeworks/hw-1-search/setup.py
@@ -0,0 +1,29 @@
+import pathlib
+from setuptools import find_packages, setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="lshifr-otus-websearch",
+    version="1.0.0",
+    description="Simple web search console interface based on one of several popular search engines. "+
+                "Homework #1 for OTUS WebPython 2020 course",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/possibly-harmless/WebPythonOTUS/tree/hw-1-search/homeworks/hw-1-search",
+    author="Leonid Shifrin",
+    license="MIT",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=["requests", "beautifulsoup4", "lxml", "click", "tabulate"],
+    entry_points={
+        "console_scripts": [
+            "websearch=search.__main__:main",
+        ]
+    },
+)
