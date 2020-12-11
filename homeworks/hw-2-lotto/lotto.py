f318a7881ea65b4c92dcc2f29c261a3b4df3080a
Leonid Shifrin <shifrinl@yandex.ru>
2020-12-11 21:01:17 +0300

    Command line interface

--- homeworks/hw-2-lotto/lotto.py
+++ homeworks/hw-2-lotto/lotto.py
@@ -1,4 +1,31 @@
-
 from lotto import Lotto
+from lotto.settings import DEFAULT_PLAYERS, MAX_PLAYERS
+import click
 
-Lotto().start_game()
+
+@click.command()
+@click.option(
+    "--name",
+    multiple=True,
+    default=[],
+    help="Имя игрока-человека. Повторяйте для каждого игрока в случае нескольких"
+)
+@click.option(
+    "--players",
+    default=DEFAULT_PLAYERS,
+    help=f"Общее число игроков. Не должно превышать максимального числа игроков {MAX_PLAYERS}"
+)
+def lotto_game(name, players):
+    if len(set(name)) < len(name):
+        print("Нельзя использовать повторяющиеся имена для игроков. Игра не может состояться.")
+        return
+    if players > MAX_PLAYERS:
+        print(
+            f"Превышено максимальное число игроков {MAX_PLAYERS}. Игра будет проходить с числом игроков {MAX_PLAYERS}"
+        )
+        players = MAX_PLAYERS
+    Lotto(player_names=name, players=players).start_game()
+
+
+if __name__ == "__main__":
+    lotto_game()
