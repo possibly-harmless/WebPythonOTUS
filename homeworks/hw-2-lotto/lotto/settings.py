202599eda1c068487d406f9582cbde48d58f248f
Leonid Shifrin <shifrinl@yandex.ru>
2020-12-11 19:48:54 +0300

    Surnames for nonhuman players. Add MAX_PLAYERS constant.

--- homeworks/hw-2-lotto/lotto/settings.py
+++ homeworks/hw-2-lotto/lotto/settings.py
@@ -2,12 +2,16 @@
 DEFAULT_MAX_NUMBER = 15
 
 USER_NAMES = (
-    "Bob",
-    "John",
-    "Lizzie",
-    "Kate",
-    "Rick",
+    "Bob Red",
+    "John Green",
+    "Liz Black",
+    "Kate White",
+    "Rick Brown",
+    "Alex Purple",
+    "Matt Orange"
 )
 
 DEFAULT_CARD_SIZE = 5
 DEFAULT_PLAYERS = 4
+
+MAX_PLAYERS = len(USER_NAMES)
